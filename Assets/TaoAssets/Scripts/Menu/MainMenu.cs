dedb9760ee3b7b5280bd309abd7bab6b407a54c5
Tenshimi Tao <158097252+Taorinio@users.noreply.github.com>
2024-04-22 21:14:15 +0300

    Controller

---
+++ Assets/TaoAssets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Canvas _MainMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            _MainMenu.gameObject.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+    public void GameStartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GameExitButton()
+    {
+        Application.Quit();
+    }
+    public void GameContinueButton() {
+        _MainMenu.gameObject.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
---
+++ Assets/TaoAssets/Scripts/Menu/SoundToggle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundToggle : MonoBehaviour
+{
+    [SerializeField] Sprite _soundOnSprite;
+    [SerializeField] Sprite _soundOffSprite;
+
+    [SerializeField] Button _soundButton;
+
+    public void SoundToggleButton()
+    {
+        if (AudioListener.volume == 0)
+        {
+            AudioListener.volume = 1;
+            _soundButton.image.sprite = _soundOnSprite;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            _soundButton.image.sprite = _soundOffSprite;
+        }
+    }
+}
