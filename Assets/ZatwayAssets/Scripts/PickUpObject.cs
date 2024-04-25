f72b3bb53bfb51320ea437507162ea24663003f5
radmir143 <142175910+radmirArs@users.noreply.github.com>
2024-04-26 01:37:00 +0500

    PickupObject
    предметам которые надо поднимать давать тег: "Pickupable".

---
+++ Assets/ZatwayAssets/Scripts/PickUpObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    public Transform handTransform; 
+    private GameObject itemInRange;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && itemInRange != null) 
+        {
+            PickupItem(); // Поднимаем предмет
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickupable")) 
+        {
+            itemInRange = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pickupable")) 
+        {
+            itemInRange = null; 
+        }
+    }
+
+    private void PickupItem()
+    {
+        itemInRange.transform.SetParent(handTransform); 
+        itemInRange.transform.localPosition = Vector3.zero; 
+        itemInRange.transform.localRotation = Quaternion.identity; 
+        itemInRange.GetComponent<Rigidbody>().isKinematic = true; 
+    }
+}
