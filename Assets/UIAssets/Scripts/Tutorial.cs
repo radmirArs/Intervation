e9308cee3e3100378b9bb54a0b068f4e3a076d90
Blacklow13 <aysel.baratova@mail.ru>
2024-05-24 19:05:01 +0300

    tutor

---
+++ Assets/UIAssets/Scripts/Tutorial.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+
+    void OnEnable()
+    {
+        Invoke("Disable",8f);
+    }
+
+    private void Disable()
+    {
+        Destroy(gameObject);
+    }
+}
